46a5a080393c365988fad9fb08240e95b5a714a2
null-federal <97811035+null-federal@users.noreply.github.com>
2022-06-21 14:06:33 -0800

    Update admins.lua

--- admins.lua
+++ admins.lua
@@ -1,1 +1,20 @@
-
+for i ,c in pairs(game.Players:GetChildren()) do
+        local playergroupid = c:GetRoleInGroup(15121704)
+        if table.find({"owner"}, playergroupid) then
+	game.StarterGui:SetCore("SendNotification", {
+		Title = "A GAYX ADMIN JOINED";
+    Text = "fed has joined";
+		Duration = 10;
+	})
+        end
+    end
+    game:GetService'Players'.PlayerAdded:Connect(function(player)
+        local playergroupid = player:GetRoleInGroup(15121704)
+        if table.find({"holder"}, playergroupid) then
+            	game.StarterGui:SetCore("SendNotification", {
+		Title = "A GAYX ADMIN JOINED";
+    Text = "lucas has joined";
+		Duration = 10;
+	})
+        end
+    end)
